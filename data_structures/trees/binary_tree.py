d6f3352fa5ff588d1f547c7765bd5ef815cb0759
KeSHaMI <pasha.mukovoz@gmail.com>
2024-07-02 17:42:35 +0300

    Reworked binary tree, delete WIP

--- data_structures/trees/binary_tree.py
+++ data_structures/trees/binary_tree.py
@@ -1,60 +1,20 @@
 from dataclasses import dataclass
-from typing import TypeVar, Generic, Optional
-from custom_types import Comparable
-from enum import Enum
-
-T = TypeVar('T', bound=Comparable)
-
-
-class DirectionEnum(str, Enum):
-    LEFT = 'left'
-    RIGHT = 'right'
+from typing import Self, Optional
 
 
 @dataclass
-class Node(Generic[T]):
+class Node[T]():
 
     value: T
-    count: int = 1
-    right: Optional['Node'[T]] = None
-    left: Optional['Node'[T]] = None
-
-    def _raise_no_direction_exception(self, direction: DirectionEnum):
-        raise ValueError(f"No direction {direction}")
-
-    def get_node_by_direction(self, direction: DirectionEnum) -> Optional[T]:
-        if direction == DirectionEnum.RIGHT:
-            return self.right
-        if direction == DirectionEnum.LEFT:
-            return self.left
-        self._raise_no_direction_exception(direction)
-
-    def add_node_by_direction(self, direction: DirectionEnum, node: 'Node'[T]) -> None:
-        if direction == direction.LEFT:
-            self.left = node
-            return
-        if direction == direction.RIGHT:
-            self.right = node
-            return
-        self._raise_no_direction_exception(direction)
-
-    def attached_nodes(self) -> list['Node'[T]]:
-        return list(
-            filter(
-                lambda item: item is not None,
-                [self.left, self.right]
-            )
-        )
-
-    def __bool__(self):
-        return self.value is not None
+    right: Optional[Self] = None
+    left: Optional[Self] = None
 
     def __str__(self):
-        return f'Node({self.value}, {self.count})'
+        return f'Node({self.value})'
 
 
 @dataclass
-class BinaryTree(Generic[T]):
+class BinaryTree[T]():
 
     root_node: Optional[Node[T]] = None
 
@@ -62,30 +22,72 @@ class BinaryTree(Generic[T]):
         if self.root_node is None:
             self.root_node = Node(value)
             return
-
         current_node = self.root_node
         while True:
-
-            if current_node.value > value:
-                direction = DirectionEnum.LEFT
-            elif current_node.value == value:
-                current_node.count += 1
-                return
-            else:
-                direction = DirectionEnum.RIGHT
-
-            next_node = current_node.get_node_by_direction(direction)
-            if next_node:
-                current_node = next_node
+            
+            if current_node.left is not None and value < current_node.left.value:
+                current_node = current_node.left
                 continue
-            current_node.add_node_by_direction(direction, Node(value))
-            return
+            if current_node.right is not None and value > current_node.right.value:
+                current_node = current_node.right
+                continue
+            
+            if value < current_node.value:
+                current_node.left = Node(value)
+                break
+            if value >= current_node.value:
+                current_node.right = Node(value)
+                break
+            
+    
+    def delete(self, value: T) -> None:
+        current_node = self.root_node
+        previous_node = None
+        while True:
+            if current_node.left is not None and value <= current_node.left.value:
+                previous_node = current_node
+                current_node = current_node.left
+                continue
+            if current_node.right is not None and value >= current_node.right.value:
+                previous_node = current_node
+                current_node = current_node.right
+                continue
+            
+            if current_node.value == value:
+                if current_node.left is not None:
+                    right_leaf = self._find_right_leaf(current_node.left)
+                    right_leaf.right = current_node.right
+                # if current_node.right is not None:
+                #     right_leaf = self._find_right_leaf(current_node.right)
+                #     right_leaf.left = current_node.right
+                
+                
+                if previous_node is not None: 
+                    previous_node.left = current_node.left
+                    return
+                self.root_node = current_node.left
+                return
+            
 
-    def delete(self, value):
-        pass
+    def __contains__(self, value: T) -> bool:
+        current_node = self.root_node
+        while True:
+            if current_node.left is not None and value <= current_node.left.value:
+                current_node = current_node.left
+                continue
+            if current_node.right is not None and value >= current_node.right.value:
+                current_node = current_node.right
+                continue
+            
+            return current_node.value == value      
 
-    def __contains__(self, item: T) -> bool:
-        pass
+    def _find_right_leaf(self, node: Node[T]) -> Node[T]:
+        current_node = node
+        while True:
+            if current_node.right is not None:
+                current_node = current_node.right
+                continue
+            return current_node
 
     def __str__(self):
         res = ''
@@ -95,3 +97,26 @@ class BinaryTree(Generic[T]):
 
         return res
 
+
+if __name__ == '__main__':
+    tree = BinaryTree()
+    tree.insert(40)
+    tree.insert(30)
+    tree.insert(25)
+    tree.insert(50)
+    tree.insert(20)
+    tree.insert(60)
+    print(tree.root_node)
+    print(tree.root_node.left)
+    print(tree.root_node.right)
+    print(tree.root_node.left.left)
+    print(tree.root_node.right.right)
+    print('deleting item')
+    tree.delete(30)
+    print(tree.root_node)
+    print(tree.root_node.left)
+    print(tree.root_node.right)
+    # print(tree.root_node.left.left)
+    print(tree.root_node.right.right)
+
+    
